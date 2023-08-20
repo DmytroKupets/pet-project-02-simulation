03184587b9ddd405a5de18963f0f43d717a90641
Dmytro <mr.vacul@gmail.com>
2023-08-20 21:27:16 +0300

    update : write abstract Predator class

--- src/main/java/com/kupets/entities/creatures/Predator.java
+++ src/main/java/com/kupets/entities/creatures/Predator.java
@@ -1,4 +1,7 @@
-package main.java.com.kupets;
+package main.java.com.kupets.entities.creatures;
+
+import main.java.com.kupets.Point;
+import main.java.com.kupets.entities.Entity;
 
 public class Predator extends Creature {
     private int attackPower;
