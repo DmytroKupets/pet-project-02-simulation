7d6bd7e7790d8dc0edefedd934defb3d5933d29a
Dmytro <mr.vacul@gmail.com>
2023-08-20 21:29:44 +0300

    fix : fix bugs

--- src/main/java/com/kupets/Map.java
+++ src/main/java/com/kupets/Map.java
@@ -1,4 +1,58 @@
 package main.java.com.kupets;
 
+import main.java.com.kupets.entities.Entity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 public class Map {
+
+    private HashMap<Point, Entity> map = new HashMap();
+    private List<Point> emptyPointsForEntity = new ArrayList<>();
+    private int width;
+    private int height;
+    public Map(int width, int height){
+        setEmptyPointsForList(width,height);
+        this.width = width;
+        this.height = height;
+    }
+    private void setEmptyPointsForList(int width, int height){
+        for (int i = 0; i < width; i++){
+            for (int j = 0; j < height; j++){
+                emptyPointsForEntity.add(new Point(i,j));
+            }
+        }
+    }
+    public Point getRandomPointFromMap(){
+        Point point = emptyPointsForEntity.get(new Random().nextInt(emptyPointsForEntity.size()));
+        emptyPointsForEntity.remove(point);
+        return point;
+    }
+    /** public void setRandomMap(Entity entity){
+        Point point = new Point(getRandomNumberForMap(widths),getRandomNumberForMap(heights));
+        entity.setPoint(point);
+        map.put(point,entity);
+    } */
+    public void entityPut(Point point, Entity entity){
+        entity.setPoint(point);
+        map.put(point,entity);
+    }
+    public String getDisplayEntity(Point keyToFind){
+        Entity value = map.get(keyToFind);
+        return value != null ? value.getTypeOfEntity() : "\uD83D\uDFEB";
+    }
+
+    public boolean isEmpty(Point keyToFind) {
+        return map.get(keyToFind) == null ? true : false;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
--- src/main/java/com/kupets/Simulation.java
+++ src/main/java/com/kupets/Simulation.java
@@ -1,4 +1,51 @@
 package main.java.com.kupets;
 
+import main.java.com.kupets.actions.Actions;
+import main.java.com.kupets.actions.initAction.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Simulation {
+    private int stepCounter = 0;
+    private Map map;
+    List<Actions> initActions = new ArrayList<>();
+    List<Actions> turnActions = new ArrayList<>();
+    Simulation(){
+        map = new Map(10,10);
+    }
+    public void nextTurn(){
+
+    }
+    public void startSimulation(){
+        createActions(map,initActions);
+        for (Actions entity : initActions){
+            entity.perform(map);
+        }
+        drowMap(map);
+    }
+    public void pauseSimulation(){
+
+    }
+
+
+    private void createActions(Map map, List<Actions> initActions){
+        initActions.add(new GrassSpawn(map));
+        initActions.add(new RockSpawn(map));
+        initActions.add(new PredatorSpawn(map));
+        initActions.add(new HerbivoreSpawn(map));
+        initActions.add(new TreeSpawn(map));
+    }
+
+    private void drowMap(Map map){
+        StringBuilder line = new StringBuilder();
+        line.append("\n");
+        for (int i = 0; i < map.getWidth(); i++){
+            for (int j = 0; j < map.getHeight(); j++){
+                line.append(map.getDisplayEntity(new Point(i,j)));
+            }
+            line.append("\n");
+        }
+        System.out.println(line);
+    }
 }
